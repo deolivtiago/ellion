9916d6e27d0e8293c9f10fd921dd72829b2e5fcf
Tiago de Oliveira <deoliv.tiago@gmail.com>
2023-10-18 23:28:10 -0300

    feat: add schema to validate user attrs

---
+++ lib/ellion_core/accounts/users/user.ex
@@ -0,0 +1,48 @@
+defmodule EllionCore.Accounts.Users.User do
+  @moduledoc """
+  Database schema for users
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias __MODULE__
+
+  @required_attrs ~w(full_name email password)a
+  @optional_attrs ~w(is_disabled)a
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  @timestamps_opts [type: :utc_datetime]
+
+  schema "users" do
+    field :full_name, :string
+    field :email, :string
+    field :password, :string, virtual: true, redact: true
+    field :password_hash, :string
+    field :is_disabled, :boolean, default: false
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs \\ %{}) do
+    user
+    |> cast(attrs, @required_attrs ++ @optional_attrs)
+    |> validate_required(@required_attrs)
+    |> unique_constraint(:id, name: :users_pkey)
+    |> unique_constraint(:email)
+    |> validate_length(:full_name, min: 2, max: 255)
+    |> validate_length(:email, min: 3, max: 255)
+    |> validate_length(:password, min: 6, max: 255)
+    |> update_change(:email, &String.downcase/1)
+    |> validate_format(:email, ~r/^[a-z0-9\-._+&#$?!]+[@][a-z0-9\-._+]+$/)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(%{valid?: true, changes: %{password: password}} = changeset) do
+    put_change(changeset, :password_hash, Argon2.hash_pwd_salt(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
+end
