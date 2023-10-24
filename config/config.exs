7e04fb4acf5349c73183af092ee51cc8b3d3c223
Tiago de Oliveira <deoliv.tiago@gmail.com>
2023-10-24 14:46:12 -0300

    chore: include support libraries

--- config/config.exs
+++ config/config.exs
@@ -23,6 +23,9 @@ config :ellion, EllionWeb.Endpoint,
   pubsub_server: EllionCore.PubSub,
   live_view: [signing_salt: "1DA6t87e"]
 
+# Configures the database timezone
+config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
