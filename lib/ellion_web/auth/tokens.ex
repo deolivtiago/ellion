f21b167b5a42e87b73fd33e8e3018a7e923f0f91
Tiago de Oliveira <deoliv.tiago@gmail.com>
2023-11-17 17:34:36 -0300

    feat: add token validation

--- lib/ellion_web/auth/tokens.ex
+++ lib/ellion_web/auth/tokens.ex
@@ -2,6 +2,7 @@ defmodule EllionWeb.Auth.Tokens do
   @moduledoc """
   Tokens management
   """
+  alias EllionCore.Accounts.Users
   alias EllionCore.Accounts.Users.User
   alias EllionWeb.Auth.Tokens.Token
 
@@ -11,13 +12,37 @@ defmodule EllionWeb.Auth.Tokens do
   ## Examples
 
     iex> generate(user)
-    {:ok, %{access: "access-token-2n13jn", refresh: "refresh-token-j31ij5"}}
+    {:ok, %{access: "access-token", refresh: "refresh-token"}}
 
   """
   def generate(%User{id: id}) do
     with {:ok, access_token, _claims} <- Token.new(id, "access"),
          {:ok, refresh_token, _claims} <- Token.new(id, "refresh") do
       {:ok, %{access: access_token, refresh: refresh_token}}
+    end
+  end
+
+  @doc """
+  Validates the given token
+
+  ## Examples
+
+      iex> validate("valid-token")
+      {:ok, %User{}}
+
+      iex> validate("invalid-token")
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def validate(token) do
+    with {:ok, %{"sub" => id}} <- Token.verify_and_validate(token),
+         {:ok, user} <- Users.get_user(:id, id) do
+      {:ok, user}
+    else
+      _error ->
+        %Ecto.Changeset{}
+        |> Ecto.Changeset.add_error(:token, "is invalid")
+        |> then(&{:error, &1})
     end
   end
 end
