7e60b4207a9bf2d97bdb573088d5e254d882c278
Tiago de Oliveira <deoliv.tiago@gmail.com>
2023-12-09 15:22:57 -0300

    feat: add migration to create user tokens table

---
+++ priv/repo/migrations/20231126151344_create_user_tokens.exs
@@ -0,0 +1,20 @@
+defmodule EllionCore.Repo.Migrations.CreateUserTokens do
+  use Ecto.Migration
+
+  def change do
+    create table(:user_tokens, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :token, :text, null: false
+      add :expiration, :timestamptz, null: false
+
+      add :user_id,
+          references(:users, on_delete: :delete_all, on_update: :update_all, type: :binary_id),
+          null: false
+
+      timestamps(type: :timestamptz)
+    end
+
+    create unique_index(:user_tokens, [:token])
+    create index(:user_tokens, [:user_id])
+  end
+end
